005cacc1aca791b0e85dd548b5fca5dbc524a6cf
yundianzixun <liangliang.zheng@idgvalue.com>
2021-12-31 17:02:38 +0800

    天气首页增加下拉刷新功能

--- daishu-weather/pages/weather/weather.js
+++ daishu-weather/pages/weather/weather.js
@@ -610,5 +610,36 @@ Page({
     wx.navigateTo({
       url: '../search/search'
     })
+  },
+  //刷新
+  onRefresh() {
+    //在当前页面显示导航条加载动画
+    wx.showNavigationBarLoading();
+    //显示 loading 提示框。需主动调用 wx.hideLoading 才能关闭提示框
+    wx.showLoading({
+      title: '加载中...',
+    })
+    this.getData();
+
+  },
+  //网络请求，获取数据
+  getData() {
+    this.setData({
+      isUpdateTime: false
+    })
+    this.getLocal();
+    //隐藏losading 提示框
+    wx.hideLoading();
+    //隐藏导航条加载动画
+    wx.hideNavigationBarLoading();
+    //停止下拉刷新
+    wx.stopPullDownRefresh();
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    //调用刷新时将执行的方法
+    this.onRefresh();
   }
 })
